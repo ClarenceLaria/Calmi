1505518bc0fed88cdd636c7f708b3354a1cc5010
ClarenceLaria <clarencelaria@gmail.com>
2025-08-09 22:06:26 +0300

    Setup questions

---
+++ lib/core/repositories/setup_questions.dart
@@ -0,0 +1,98 @@
+
+final List<Map<String, dynamic>> setupQuestions = [
+  {
+    "question": "What should we call you?",
+    "description": "First things first, what should we call you?",
+  },
+  {
+    "question": "What is your gender?",
+    "description": "Help us understand you better by selecting your gender.",
+    "options": [
+      "Male",
+      "Female",
+    ]
+  },
+  {
+    "question": "How old are you?",
+    "description": "Your age helps us tailor our recommendations for you.",
+  },
+  {
+    "question": "What is your main goal?",
+    "description": "Select the option that best represents what you're hoping to achieve (select all that apply).",
+    "options": [
+      "Manage Anxiety",
+      "Reduce Stress",
+      "Improve Mood",
+      "Improve Sleep",
+      "Enhance Relationships",
+      "Boost Confidence",
+      "Enhance Focus",
+    ]
+  },
+  {
+    "question": "What causes your mental health issues?",
+    "description": "What factors contribute to your current mental health challenges? (select all that apply).",
+    "options": [
+      "Work/school",
+      "Relationships",
+      "Finances",
+      "Health Concerns",
+      "Life Changes",
+      "Other"
+    ]
+  },
+  {
+    "question": "How often did you feel stressed or anxious in the last 12 months?",
+    "description": "In the past year, how often have you experienced feelings of stress or anxiety?",
+    "options": [
+      "Almost Daily",
+      "Frequently",
+      "Occasionally",
+      "Rarely",
+      "Never"
+    ]
+  },
+  {
+    "question": "Do you eat healthily?",
+    "description": "How do you describe your eating habits? Your diet can significantly impact your mental well-being.",
+    "options": [
+      "Always",
+      "Most of the time",
+      "Sometimes",
+      "Rarely",
+      "Never",
+    ]
+  },
+  {
+    "question": "Have you ever tried meditation before?",
+    "description": "Have you ever practiced meditation or mindfulness techniques before?",
+    "options": [
+      "Yes, regularly",
+      "Yes, occasionally",
+      "Yes, a long time ago",
+      "No, I have never tried meditation",
+    ]
+  },
+  {
+    "question": "Have would you rate your level of sleep quality overall?",
+    "description": "Understand your sleep quality helps us provide personalized support.",
+    "options": [
+      "Very Poor",
+      "Poor",
+      "Average",
+      "Good",
+      "Excellent",
+    ]
+  },
+  {
+    "question": "How qould you rate your level of happiness overall?",
+    "description": "Understanding your happiness level helps us personalize support.",
+    "options": [
+      "Very Unhappy",
+      "Unhappy",
+      "Neutral",
+      "Happy",
+      "Very Happy",
+    ]
+  },
+];
